bafa9a24b3a8a298a23f21fef506cab6afadc818
Christian Fleschhut <weltrekordhalter@gmail.com>
2022-01-25 23:28:45 +0100

    Add second (unit) test suite and implementation

---
+++ src/anagrams.test.js
@@ -0,0 +1,27 @@
+import { expect } from 'chai';
+import { isAnagram } from './anagrams';
+
+describe('isAnagram basic functionality', () => {
+  it('returns true when two known anagrams are passed in', () => {
+    const expected = true;
+    const actual = isAnagram('listen', 'silent');
+
+    expect(actual).to.equal(expected);
+  });
+
+  it('returns false when either of the strings has extra letters', () => {
+    const expected = false;
+    const actual = isAnagram('elbows', 'below');
+    const actual2 = isAnagram('below', 'elbows');
+
+    expect(actual).to.equal(expected);
+    expect(actual2).to.equal(expected);
+  });
+
+  it('returns false when the strings have the same letters in different quantities', () => {
+    const expected = false;
+    const actual = isAnagram('listens', 'silent');
+
+    expect(actual).to.equal(expected);
+  });
+});
