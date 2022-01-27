6e65d59ffc22c33cb68f931863c2a68d213a071c
Christian Fleschhut <weltrekordhalter@gmail.com>
2022-01-27 19:40:58 +0100

    Move resetting of db into afterEach hook

--- src/db.test.js
+++ src/db.test.js
@@ -7,6 +7,10 @@ import {
 } from './test-helpers';
 
 describe('getUserByUsername', () => {
+  afterEach('reset the database', async () => {
+    await resetDatabase();
+  });
+
   it('gets the correct user from the database given a username', async () => {
     const fakeData = [
       {
@@ -25,7 +29,6 @@ describe('getUserByUsername', () => {
 
     const actual = await getUserByUsername('abc');
     const finalDBState = await getDatabaseData('users');
-    await resetDatabase();
 
     const expected = {
       id: '123',
